011d95878bea412c408bdb2402ee8e6df50d0191
Bobby Jack <bobbyjack@gmail.com>
2019-05-05 19:58:12 +0100

    rudimentary stats-gathering

---
+++ js/hit.js
@@ -0,0 +1,40 @@
+postAjax('http://37.139.1.25:82/hit.php', 'url=https://bobbyjack.me');
+
+function postAjax(url, data) {
+    var params = typeof data == 'string' ? data : Object.keys(data).map(
+            function(k){ return encodeURIComponent(k) + '=' + encodeURIComponent(data[k]) }
+        ).join('&');
+
+    //var xhr = window.XMLHttpRequest ? new XMLHttpRequest() : new ActiveXObject("Microsoft.XMLHTTP");
+
+    var xhr = createCORSRequest('post', url);
+
+    //xhr.open('POST', url);
+
+    /*xhr.onreadystatechange = function() {
+        if (xhr.readyState>3 && xhr.status==200) { success(xhr.responseText); }
+    };*/
+
+    xhr.setRequestHeader('X-Requested-With', 'XMLHttpRequest');
+    xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+    xhr.send(params);
+    return xhr;
+}
+
+function createCORSRequest(method, url){
+    var xhr = new XMLHttpRequest();
+
+    if ("withCredentials" in xhr) {
+// XHR has 'withCredentials' property only if it supports CORS
+        xhr.open(method, url, true);
+    } else if (typeof XDomainRequest != "undefined") {
+// if IE use XDR
+        xhr = new XDomainRequest();
+        xhr.open(method, url);
+    } else {
+        xhr = null;
+    }
+
+    return xhr;
+}
+
