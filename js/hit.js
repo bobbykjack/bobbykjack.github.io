e7ae9c5f0991a977c6c29b71067994c2afc7e9ed
Bobby Jack <bobbyjack@gmail.com>
2019-05-06 16:29:35 +0100

    more stats

--- js/hit.js
+++ js/hit.js
@@ -1,19 +1,11 @@
-postAjax('https://stats.bobbyjack.me/hit.php', 'url=https://bobbyjack.me');
+postAjax('https://stats.bobbyjack.me/hit.php', { url: window.location });
 
 function postAjax(url, data) {
     var params = typeof data == 'string' ? data : Object.keys(data).map(
             function(k){ return encodeURIComponent(k) + '=' + encodeURIComponent(data[k]) }
         ).join('&');
 
-    //var xhr = window.XMLHttpRequest ? new XMLHttpRequest() : new ActiveXObject("Microsoft.XMLHTTP");
-
     var xhr = createCORSRequest('post', url);
-
-    //xhr.open('POST', url);
-
-    /*xhr.onreadystatechange = function() {
-        if (xhr.readyState>3 && xhr.status==200) { success(xhr.responseText); }
-    };*/
 
     xhr.setRequestHeader('X-Requested-With', 'XMLHttpRequest');
     xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
