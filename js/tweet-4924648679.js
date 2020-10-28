62066102ba1a969514378fbf61ce5ce27ae6154a
Bobby Jack <bobbyjack@gmail.com>
2020-10-28 13:53:07 +0000

    small updates

---
+++ js/tweet-4924648679.js
@@ -0,0 +1,24 @@
+window.twttr = (function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0],
+    t = window.twttr || {};
+  if (d.getElementById(id)) return t;
+  js = d.createElement(s);
+  js.id = id;
+  js.src = "https://platform.twitter.com/widgets.js";
+  js.onload = go;
+  fjs.parentNode.insertBefore(js, fjs);
+
+  t._e = [];
+  t.ready = function(f) {
+    t._e.push(f);
+  };
+
+  return t;
+}(document, "script", "twitter-wjs"));
+
+function go() {
+    twttr.widgets.createTweet(
+        '4924648679',
+        document.getElementById('tweet')
+    );
+}
