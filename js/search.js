e54840c3794e2a04be28a3f2be4cab8af39097be
Bobby Jack <bobbyjack@gmail.com>
2021-02-07 14:52:53 +0000

    more cleanup

--- js/search.js
+++ js/search.js
@@ -8,15 +8,26 @@ var search_button = document.querySelector("#search button");
 var sitemap = {};
 var excludes = [];
 
+
 if (search_button) {
-    setupEvent();
+    search_button.parentNode.removeChild(search_button);
 
 // Load sitemap
     var request = new XMLHttpRequest();
 
     request.addEventListener("load", function() {
         sitemap = JSON.parse(this.responseText);
-        //console.log(sitemap);
+
+// if query string
+        var curr = new URL(window.location);
+        var query = curr.searchParams.get("q");
+
+        if (query) {
+            document.querySelector("#search input[name='q']").value = query;
+            searchFor(query);
+        }
+
+        setupEvent();
     });
 
     request.open("GET", "/js/json-ld.json");
@@ -28,64 +39,60 @@ if (search_button) {
 function setupEvent() {
     var text = document.querySelector("#search input[name='q']");
 
-    //search_button.addEventListener("click", function(ev) {
     text.addEventListener("input", function(ev) {
         ev.preventDefault();
-
-        if (Object.keys(sitemap).length == 0) {
-            console.log("nothing in sitemap yet...");
-        }
-
-        var matches = [];
-        var str;
-
-        if (str = text.value.trim()) {
-            str = str.toLowerCase();
-
-// Scan sitemap for matches
-            for (var path in sitemap) {
-                var page = sitemap[path];
-                page.path = path;
-
-                if (excludes.includes(path)) {
-                    continue;
-                }
-
-                if (path.indexOf(str) !== -1) {
-                    matches.push(page);
-                } else if (page.hasOwnProperty("name")
-                    && page.name.toLowerCase().indexOf(str) !== -1
-                ) {
-                    matches.push(page);
-                } else if (page.hasOwnProperty("abstract")
-                    && page.abstract.toLowerCase().indexOf(str) !== -1
-                ) {
-                    matches.push(page);
-                } else if (page.hasOwnProperty("itemReviewed")
-                    && page.itemReviewed.hasOwnProperty("reviewBody")
-                    && page.itemReviewed.reviewBody.toLowerCase().indexOf(str)
-                        !== -1
-                ) {
-                    matches.push(page);
-                }
-            }
-        }
-
-// Add results to page
-        if (1 || matches.length) {
-            //console.log(str);
-            insertResults(matches, str);
-        }
-
-        ev.preventDefault();
+        searchFor(text.value.trim());
     });
 }
 
 /**
  */
-function insertResults(results, original_query) {
-    //console.log(results);
+function searchFor(str) {
+    if (Object.keys(sitemap).length == 0) {
+        console.log("nothing in sitemap yet...");
+    }
 
+    var matches = [];
+
+    if (str) {
+        str = str.toLowerCase();
+
+// Scan sitemap for matches
+        for (var path in sitemap) {
+            var page = sitemap[path];
+            page.path = path;
+
+            if (excludes.includes(path)) {
+                continue;
+            }
+
+            if (path.indexOf(str) !== -1) {
+                matches.push(page);
+            } else if (page.hasOwnProperty("name")
+                && page.name.toLowerCase().indexOf(str) !== -1
+            ) {
+                matches.push(page);
+            } else if (page.hasOwnProperty("abstract")
+                && page.abstract.toLowerCase().indexOf(str) !== -1
+            ) {
+                matches.push(page);
+            } else if (page.hasOwnProperty("itemReviewed")
+                && page.itemReviewed.hasOwnProperty("reviewBody")
+                && page.itemReviewed.reviewBody.toLowerCase().indexOf(str)
+                    !== -1
+            ) {
+                matches.push(page);
+            }
+        }
+    }
+
+// Add results to page
+    insertResults(matches, str);
+}
+
+/**
+ */
+function insertResults(results, original_query) {
     var results_el = document.getElementById("results"),
         result;
 
@@ -99,13 +106,22 @@ function insertResults(results, original_query) {
         //heading.appendChild(document.createTextNode("Results"));
         //results_el.appendChild(heading);
         results_el.setAttribute("id", "results");
-        search_form.parentNode.insertBefore(results_el, search_form.nextSibling);
+
+        search_form.parentNode.insertBefore(
+            results_el,
+            search_form.nextSibling
+        );
     }
 
     if (results.length) {
         var heading = document.createElement("h2");
         heading.appendChild(document.createTextNode("Results"));
         results_el.appendChild(heading);
+    } else {
+        var italic = document.createElement("i");
+        var none = "No results found for that search";
+        italic.appendChild(document.createTextNode(none));
+        results_el.appendChild(italic);
     }
 
     for (var i = 0; i < results.length; i++) {
@@ -151,7 +167,11 @@ function insertResults(results, original_query) {
 
         if (results[i].hasOwnProperty("datePublished")) {
             var small = document.createElement("small");
-            small.appendChild(document.createTextNode(results[i].datePublished));
+            
+            small.appendChild(document.createTextNode(
+                results[i].datePublished
+            ));
+            
             article.appendChild(small);
         }
 
