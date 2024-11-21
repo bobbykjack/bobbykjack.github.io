5e7498e9a0646ba82424e890cbfd366168efb774
Bobby Jack <bobbyjack@gmail.com>
2024-11-21 21:48:21 +0000

    small bug fix: make text search case-insensitive

--- js/table.js
+++ js/table.js
@@ -3,56 +3,55 @@
 //------------------------------------------------------------------------------
 
 (function () {
-  var tbl = document.getElementById("switch-progress"),
-    formNode = document.createElement("form");
-  (inputNode = document.createElement("input")),
-    (params = new URLSearchParams(window.location.search));
+    var tbl = document.getElementById("switch-progress"),
+        formNode = document.createElement("form");
+    (inputNode = document.createElement("input")),
+        (params = new URLSearchParams(window.location.search));
 
-  var rows, title, searchTitle;
+    var rows, title, searchTitle;
 
-  inputNode.setAttribute("type", "text");
-  inputNode.setAttribute("name", "title");
-  inputNode.setAttribute("autofocus", "1");
-  inputNode.setAttribute("placeholder", "Search...");
+    inputNode.setAttribute("type", "text");
+    inputNode.setAttribute("name", "title");
+    inputNode.setAttribute("autofocus", "1");
+    inputNode.setAttribute("placeholder", "Search...");
 
-  formNode.appendChild(inputNode);
-  formNode.setAttribute("id", "search");
+    formNode.appendChild(inputNode);
+    formNode.setAttribute("id", "search");
 
-  tbl.parentNode.insertBefore(formNode, tbl);
+    tbl.parentNode.insertBefore(formNode, tbl);
 
-  setupEvent(inputNode);
+    setupEvent(inputNode);
 
-  if (params.has("title")) {
-    var title = params.get("title");
-    inputNode.setAttribute("value", title);
-    searchTitle = title.toLowerCase();
-    searchFor(searchTitle);
-  }
-
-  /**
-   */
-  function searchFor(text) {
-    rows = tbl.querySelectorAll("tbody tr");
-
-    for (var i = 0; i < rows.length; i++) {
-      title = rows[i].firstElementChild.firstChild.textContent.toLowerCase();
-
-      if (title.indexOf(text) === -1) {
-        rows[i].style.display = "none";
-      } else {
-        rows[i].style.display = "";
-      }
+    if (params.has("title")) {
+        var title = params.get("title");
+        inputNode.setAttribute("value", title);
+        searchTitle = title.toLowerCase();
+        searchFor(searchTitle);
     }
-  }
 
-  /**
-   */
-  function setupEvent(node) {
-    //var text = document.querySelector("#search input[name='title']");
+    /**
+     */
+    function searchFor(text) {
+        rows = tbl.querySelectorAll("tbody tr");
 
-    node.addEventListener("input", function (ev) {
-      ev.preventDefault();
-      searchFor(node.value.trim());
-    });
-  }
+        for (var i = 0; i < rows.length; i++) {
+            title =
+                rows[i].firstElementChild.firstChild.textContent.toLowerCase();
+
+            if (title.indexOf(text) === -1) {
+                rows[i].style.display = "none";
+            } else {
+                rows[i].style.display = "";
+            }
+        }
+    }
+
+    /**
+     */
+    function setupEvent(node) {
+        node.addEventListener("input", function (ev) {
+            ev.preventDefault();
+            searchFor(node.value.trim().toLowerCase());
+        });
+    }
 })();
